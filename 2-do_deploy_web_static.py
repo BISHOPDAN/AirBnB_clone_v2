bdfed3a7e68878240a925be57e4c7046160fa569
BISHOPDAN <danieljulius554@gmail.com>
2023-01-30 16:10:51 +0000

    made some currections

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from os import path
 
 
-env.hosts = ['100.25.19.204', '54.157.159.85']
+env.hosts = ['52.201.151.80', '54.82.221.248']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/id_rsa'
 
